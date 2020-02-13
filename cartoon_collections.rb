b8b26e312e5755fc870ca561e669d0f48d29c976
Lester Young <lyoung070789@gmail.com>
2020-02-13 20:07:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,7 @@ end
 #use .find on passed argument. Within this block we must take each index and see if it is included in the given array cheese_types. 
 #use .include? on cheese_types passing through the argument string, from the main argument array. 
 #.include returns true or false on each index, once it finds one true. .find method will return that string.
+
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find{|string| cheese_types.include?(string)}
